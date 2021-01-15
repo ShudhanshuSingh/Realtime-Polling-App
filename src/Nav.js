29255e40938dbf355c3745270b39ea59216e1f36
ShudhanshuSingh <shudhanshusingh051@gmail.com>
2021-01-15 22:36:54 +0530

    navbar and its navigation fixed

--- src/Nav.js
+++ src/Nav.js
@@ -1,23 +1,38 @@
 import React from 'react'
 import pollLogo from './pollnewlogo.png'
 import "./Nav.css"
+import {IconButton} from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import {Link,useHistory} from "react-router-dom"
 function Nav({backbutton}) {
     const history  = useHistory()
+    const moveBack = () =>{
+        history.goBack();
+    }
+    const logoClick = () =>{
+        history.push("/");
+    }
     return (
         <div className="nav-bar">
             {
                 backbutton?(
-                        <img className="back-arrow" src="https://img.icons8.com/material-sharp/24/000000/back--v1.png"/>
+                    <IconButton onClick={moveBack} className="back-arrow">
+                        <ArrowBackIcon />
+                    </IconButton>
+                        
                 ):(
-                    <img src={pollLogo} alt="Poll" className="app-logo" />
+                <a href="/">
+                    <img src={pollLogo} alt="Poll" className="app-logo"  />
+                </a>
                 )
             }
             <div className="nav-features">
-                <h3>Word Cloud</h3>
+            <button className="nav-button">
+                <h3>About</h3>
+            </button>
             </div>
         </div>
     )
 }
 
-export default Nav
+export default Nav;
